64361cb031416761c85cd78c3167093acfed51ff
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-09-19 17:34:55 -0400

    clean up

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -24,8 +24,6 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Full stack web developer, VC, NFT Trader, Founder, Writer, Content Creator, and serial learner of high impact skills.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
