3e3a24f6abd334b57839567e74de83ef028c5bcf
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-09-18 20:21:13 -0400

    mac test

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Full stack web developer, VC, NFT Trader, Founder, Writer, Content Creator, and serial learner of high impact skills.</p>
+        <p>Full stack web developer, VC, NFT Trader, Founder, Writer, Content Creator, and serial learner of high impact skills. test</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
